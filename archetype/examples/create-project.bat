62a2151468156f02e754c9fc7f0b4843505b240e
Tomas Cejka <tomas.cejka@styrax.cz>
2021-07-27 23:35:29 +0200

    * uprava archetype pomocných skritpů

--- archetype/examples/create-project.bat
+++ archetype/examples/create-project.bat
@@ -5,12 +5,19 @@ REM Vytvori podadresar se jmenem; kasli na diakritiku a mezery
 REM
 
 :INPUT_DIRNAME
-SET /P dname=Set dirname value: 
+ECHO ------------------------------------------------------
+ECHO   Create project (archetype)
+ECHO ------------------------------------------------------
+IF [%1]==[] SET /P artifactID= Set artifactID value:
+IF [%2]==[] SET /P groupID= Set groupId value:
 
-call ..\quickstart\create-archetype.bat %dname%
-copy ..\quickstart\modify-pom.bat .\%dname%
-cd %dname%
-call .\modify-pom.bat .
-rm modify-pom.bat
+mkdir %artifactID%
 
+call ..\quickstart\create-archetype.bat %artifactID% %groupID%
+copy ..\quickstart\modify-pom.bat .\%artifactID%
+
+call .\modify-pom.bat %artifactID% %groupID%
+rm %artifactID%\modify-pom.bat
+
+pause
 :eof
--- archetype/quickstart/use-archetype.bat
+++ archetype/quickstart/use-archetype.bat
@@ -3,11 +3,14 @@
 REM
 REM Vygeneruje maven projekt v adresáři ze šablony (archetype) z lokálního repositáře. Kde skript spustíte, založí podadresář
 REM s názvem v proměnné 'artifactID'. Skript předpokládá, že příkaz 'mvn' je spustitelný a dohledatelný na PATH.
+REM @author tomas.cejka
 REM
 
-SET /P groupID=Set groupId value: 
-SET /P artifactID=Set artifactID value:
+SET artifactID=%1
+SET groupID=%2
 SET version=1.0.0-SNAPSHOT
+IF[%1]==[] SET /P artifactID=Set artifactID value:
+IF[%2]==[] SET /P groupID=Set groupId value:
 REM Je mozne: internal, local, remote; lze pouzivat vice volbe oddelene carkou, napr: remote,local
 SET catalog=local
 
